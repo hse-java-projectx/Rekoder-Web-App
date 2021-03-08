d1c8e232df6b64935cbcdfafcd88cac1ab82a936
Glebanister <glebmar2001@gmail.com>
2021-03-08 18:03:47 +0300

    Remove debug from Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,25 +3,3 @@
     <h1>This is Profile Page of {{ user.name }}</h1>
   </div>
 </template>
-
-<script>
-import api from '../js/api';
-import state from '../router/index';
-
-export default {
-  name: 'Profile',
-  props: {
-    userIdentifier: String,
-  },
-  data() {
-    return {
-      user: null,
-    };
-  },
-  components: {},
-  beforeMount() {
-    console.log(state.state);
-    this.user = api.api.getUser(this.userIdentifier);
-  },
-};
-</script>
