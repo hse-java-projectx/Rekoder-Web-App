1aed1b264961a101d49af75e860d55e9e4bd5159
Glebanister <glebmar2001@gmail.com>
2021-03-08 17:51:49 +0300

    Optimize navbar for narrow screen

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,111 +1,91 @@
 <template>
-  <!-- <b-row class="justify-content-center m-0 p-0">
-    <b-col cols="12" xl="10" class="p-0"> -->
-  <div class="comp-navbar-wrapper">
-    <b-navbar toggleable="md" type="dark" variant="primary" class="comp-navbar">
-      <b-navbar-brand>
-        <router-link to="/feed" class="header-item d-inline-block d-md-none">
-          <h3>Rekoder</h3>
-        </router-link>
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav class="w-100">
-        <b-container fluid>
-          <b-navbar-nav class="w-100">
-            <b-row class="w-100">
-              <b-col md="8">
-                <b-row>
-                  <b-col class="navbar-item p-md-0" cols="12" md="3" xl="2">
-                    <b-nav-item>
-                      <router-link to="/feed" class="header-item">
-                        <b-icon
-                          icon="newspaper"
-                          class="d-md-none d-lg-inline"
-                        ></b-icon>
-                        Feed
-                      </router-link>
-                    </b-nav-item>
-                  </b-col>
-                  <b-col class="navbar-item p-md-0" cols="12" md="3" xl="2">
-                    <b-nav-item>
-                      <router-link to="/explore" class="header-item">
-                        <b-icon
-                          icon="signpost"
-                          class="d-md-none d-lg-inline"
-                        ></b-icon>
-                        Explore
-                      </router-link>
-                    </b-nav-item>
-                  </b-col>
-                  <b-col cols="12" md="6">
-                    <b-form-input placeholder="Search" />
-                  </b-col>
-                </b-row>
-              </b-col>
+  <b-navbar toggleable="md" type="dark" variant="primary" class="comp-navbar">
+    <b-navbar-brand>
+      <router-link
+        to="/feed"
+        class="navbar-brand-text d-inline-block d-md-none"
+      >
+        Rekoder
+      </router-link>
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav class="w-100">
+      <b-container fluid>
+        <b-navbar-nav class="w-100">
+          <b-row class="w-100">
+            <b-col md="8">
+              <b-row>
+                <b-col class="navbar-link" cols="12" md="3" xl="2">
+                  <NavbarLink name="Feed" link="/feed" icon="newspaper" />
+                </b-col>
+                <b-col class="navbar-link" cols="12" md="3" xl="2">
+                  <NavbarLink name="Explore" link="/explore" icon="signpost" />
+                </b-col>
+                <b-col cols="12" md="6" class="my-auto">
+                  <b-form-input size="sm" placeholder="Search" />
+                </b-col>
+              </b-row>
+            </b-col>
 
-              <b-col md="4">
-                <b-row align-h="end">
-                  <b-col
-                    class="navbar-item p-md-0"
-                    cols="12"
-                    md="4"
-                    lg="6"
-                    xl="4"
-                  >
-                    <b-nav-item>
-                      <router-link to="/profile" class="header-item">
-                        <b-icon
-                          icon="person"
-                          class="d-md-none d-lg-inline"
-                        ></b-icon>
-                        Profile
-                      </router-link>
-                    </b-nav-item>
-                  </b-col>
-                  <b-col
-                    class="navbar-item p-md-0"
-                    cols="12"
-                    md="4"
-                    lg="6"
-                    xl="4"
-                  >
-                    <b-nav-item>
-                      <router-link to="/archive" class="header-item">
-                        <b-icon
-                          icon="collection"
-                          class="d-md-none d-lg-inline"
-                        ></b-icon>
-                        Archive
-                      </router-link>
-                    </b-nav-item>
-                  </b-col>
-                </b-row>
-              </b-col>
-            </b-row>
-          </b-navbar-nav>
-        </b-container>
-      </b-collapse>
-    </b-navbar>
-  </div>
-  <!-- </b-col>
-  </b-row> -->
+            <b-col md="4">
+              <b-row align-h="end">
+                <b-col
+                  class="navbar-link p-md-0"
+                  cols="12"
+                  md="4"
+                  lg="6"
+                  xl="4"
+                >
+                  <NavbarLink name="Profile" link="/profile" icon="person" />
+                </b-col>
+                <b-col
+                  class="navbar-link p-md-0"
+                  cols="12"
+                  md="4"
+                  lg="6"
+                  xl="4"
+                >
+                  <NavbarLink
+                    name="Archive"
+                    link="/archive"
+                    icon="collection"
+                  />
+                </b-col>
+              </b-row>
+            </b-col>
+          </b-row>
+        </b-navbar-nav>
+      </b-container>
+    </b-collapse>
+  </b-navbar>
 </template>
+
+<script>
+import NavbarLink from '@/components/NavbarLink.vue';
+
+export default {
+  components: {
+    NavbarLink,
+  },
+};
+</script>
 
 <style scoped lang="sass">
 @import src/style/bootstrap-custom.scss
 @import bootstrap/scss/bootstrap
 
-.comp-navbar-wrapper
-  position: sticky
-  top: 0
+@media (min-width: $grid-md)
+  .navbar-link
+    max-width: 80pt
 
-.navbar-item
-  max-width: 80pt
-
-.header-item
+.navbar-link-text
   color: $light
-  font-size: 13pt
+  &:hover
+    text-decoration-line: underline
+
+.navbar-brand-text
+  color: $light
   &:hover
     color: $light
-    underline: none
+    text-decoration: none
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="page-profile">
-    <h1>This is Profile Page</h1>
+  <div>
+    <h1>This is Profile Page of {{ user.name }}</h1>
   </div>
 </template>
 
 <script>
+import api from '../js/api';
+import state from '../router/index';
+
 export default {
   name: 'Profile',
+  props: {
+    userIdentifier: String,
+  },
+  data() {
+    return {
+      user: null,
+    };
+  },
   components: {},
+  beforeMount() {
+    console.log(state.state);
+    this.user = api.api.getUser(this.userIdentifier);
+  },
 };
 </script>
