cec2c7b24691f5251acad87e943ce2137f889246
Glebanister <glebmar2001@gmail.com>
2021-03-13 01:20:20 +0300

    Implement archive layout

---
+++ src/components/archive/DirectoryItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-list-group-item class="d-flex justify-content-between">
+    <div>
+      <span class="mr-2">
+        <b-icon v-if="isDirectory" icon="folder"></b-icon>
+        <b-icon v-else-if="solved" icon="check-circle" class="solved"></b-icon>
+        <b-icon v-else icon="x-circle" class="attempted"></b-icon>
+      </span>
+      <router-link :to="link"> {{ name }} </router-link>
+    </div>
+    <b-icon icon="gear" class="text-secondary" />
+  </b-list-group-item>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    isDirectory: Boolean,
+    link: String,
+    solved: Boolean,
+  },
+  computed: {},
+};
+</script>
+
+<style scoped lang="sass">
+@import src/style/bootstrap-custom.scss
+@import bootstrap/scss/bootstrap
+
+.solved
+  color: $green
+
+.attempted
+  color: $red
+</style>
--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -1,12 +1,131 @@
 <template>
   <div>
-    <h1>This is Archive Page</h1>
+    <SubNavbar>
+      <template #left>
+        <span
+          class="d-none d-md-inline-block"
+          v-for="way in path"
+          :key="way.name"
+        >
+          <span class="path-way">
+            <router-link class="path-way-link" :to="way.link">
+              {{ way.name }}
+            </router-link>
+            /
+          </span>
+        </span>
+        <span class="d-inline-block d-md-none">
+          <span v-if="hasPrevWay" class="path-way">
+            <router-link class="path-way-link" :to="prevWay.link">
+              ...
+            </router-link>
+            /
+          </span>
+          <span class="path-way">
+            <router-link class="path-way-link" :to="lastWay.link">
+              {{ lastWay.name }}
+            </router-link>
+            /
+          </span>
+        </span>
+      </template>
+      <template #right>
+        <b-icon icon="folder-plus" class="text-primary" />
+      </template>
+    </SubNavbar>
+    <div class="page-item-container">
+      <b-list-group>
+        <DirectoryItem
+          v-for="item in directions"
+          :key="item.name"
+          :name="item.name"
+          :isDirectory="item.isDirectory"
+          :link="item.link"
+          :solved="item.solved"
+        ></DirectoryItem
+      ></b-list-group>
+    </div>
   </div>
 </template>
 
 <script>
+import DirectoryItem from '@/components/archive/DirectoryItem.vue';
+import SubNavbar from '@/components/SubNavbar.vue';
+
 export default {
   name: 'Archive',
-  components: {},
+  data() {
+    return {
+      owner: 'Gleb Marin',
+      path: [
+        { name: 'Glebanister', link: 'user/Glebanister' },
+        { name: 'root', link: 'user/Glebanister/root' },
+        { name: 'collections', link: 'user/Glebanister/root/collections' },
+      ],
+      directions: [
+        {
+          name: 'leetcode',
+          isDirectory: true,
+          link: '/leetcode',
+          solved: false,
+        },
+        {
+          name: 'codefoeces',
+          isDirectory: true,
+          link: '/codefoeces',
+          solved: true,
+        },
+        {
+          name: 'A + B Problem',
+          isDirectory: false,
+          link: '/aplusb',
+          solved: true,
+        },
+        {
+          name: 'Find path',
+          isDirectory: false,
+          link: '/findpath',
+          solved: false,
+        },
+        {
+          name: '3-SAT',
+          isDirectory: false,
+          link: '/3sat',
+          solved: false,
+        },
+        {
+          name: 'collection',
+          isDirectory: true,
+          link: '/collection',
+          solved: null,
+        },
+      ],
+    };
+  },
+  components: { DirectoryItem, SubNavbar },
+  computed: {
+    lastWay() {
+      return this.path[this.path.length - 1];
+    },
+    hasPrevWay() {
+      return this.path.length >= 2;
+    },
+    prevWay() {
+      return this.path[this.path.length - 2];
+    },
+  },
 };
 </script>
+
+<style scoped lang="sass">
+@import src/style/bootstrap-custom.scss
+@import bootstrap/scss/bootstrap
+
+.path-way
+  font-size: 18pt
+
+.path-way-link
+  color: $secondary
+  &:hover
+    color: $dark
+</style>
